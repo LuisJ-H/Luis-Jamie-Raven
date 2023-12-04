55c5df14791c10654093f03765325c0ade952d40
Luis Hernandez <149843869+LuisJ-H@users.noreply.github.com>
2023-12-04 15:37:14 -0500

    Add files via upload

---
+++ Cart/cart.js
@@ -0,0 +1,257 @@
+import {foodItem} from './fooditem.js'
+
+function displayItems(){
+    let rice= document.getElementById('rice');
+    let appetizer=  document.getElementById('appetizer');;
+    let entree=  document.getElementById('entree');
+    let dessert=  document.getElementById('dessert');
+    let drinks=  document.getElementById('drinks');
+
+
+    const riceData= foodItem.filter((item)=>item.category=='rice');
+    const entreeData= foodItem.filter((item)=>item.category=='entree');
+    const appetizerData= foodItem.filter((item)=>item.category=='appetizer');
+    const dessertData= foodItem.filter((item)=>item.category=='dessert');
+    const drinksData= foodItem.filter((item)=>item.category=='drinks');
+    riceData.map(item=>{
+        
+        let cardContainer= document.createElement('div');
+        cardContainer.setAttribute('id','card-container')
+
+        let cardTop= document.createElement('div');
+        cardTop.setAttribute('id','card-top');
+        
+        let cardImg= document.createElement('div');
+        cardImg.setAttribute('id','card-img');
+
+        let img= document.createElement('img');
+        img.src=item.img;
+        
+        
+        let star= document.createElement('i');
+        star.setAttribute('class','bi bi-star-fill');
+        star.setAttribute('id','rating');
+        star.innerText= ' ' + item.rating;
+        
+        let heart= document.createElement('ion-icon');
+        heart.setAttribute('class','heart-outline');
+        heart.setAttribute('id',item.id)
+
+        cardTop.appendChild(star);
+        cardTop.appendChild(heart);
+        
+        cardImg.appendChild(img);
+        
+        let itemName= document.createElement('p');
+        itemName.setAttribute('id','item-name');
+        itemName.innerText= item.name;
+        
+        let itemPrice= document.createElement('p');
+        itemPrice.setAttribute('id','item-price');
+        itemPrice.innerText= 'Price : $ ' + item.price;
+
+        cardContainer.appendChild(cardTop);
+        cardContainer.appendChild(cardImg);
+        cardContainer.appendChild(itemName);
+        cardContainer.appendChild(itemPrice);
+
+        rice.appendChild(cardContainer);
+        
+    })
+
+
+    entreeData.map(item=>{
+        let cardContainer= document.createElement('div');
+        cardContainer.setAttribute('id','card-container')
+
+        let cardTop= document.createElement('div');
+        cardTop.setAttribute('id','card-top');
+
+        let cardImg= document.createElement('div');
+        cardImg.setAttribute('id', 'card-img');
+
+        let img= document.createElement('img');
+        img.src=item.img;
+
+        let star= document.createElement('i');
+        star.setAttribute('class','bi bi-star-fill');
+        star.setAttribute('id','rating');
+        star.innerText= ' ' + item.rating;
+
+        let heart= document.createElement('ion-icon');
+        heart.setAttribute('class','heart-outline');
+        heart.setAttribute('id',item.id)
+
+        cardTop.appendChild(star);
+        cardTop.appendChild(heart);
+
+        cardImg.appendChild(img);
+
+
+        let itemName= document.createElement('p');
+        itemName.setAttribute('id','item-name');
+        itemName.innerText= item.name;
+
+        let itemPrice= document.createElement('p');
+        itemPrice.setAttribute('id','item-price');
+        itemPrice.innerText= 'Price : $ ' + item.price;
+
+        cardContainer.appendChild(cardTop);
+        cardContainer.appendChild(cardImg);
+        cardContainer.appendChild(itemName);
+        cardContainer.appendChild(itemPrice);
+        
+        entree.appendChild(cardContainer);
+
+    })
+
+    appetizerData.map(item=>{
+        let cardContainer= document.createElement('div');
+        cardContainer.setAttribute('id','card-container')
+
+        let cardTop= document.createElement('div');
+        cardTop.setAttribute('id','card-top');
+
+        let cardImg= document.createElement('div');
+        cardImg.setAttribute('id', 'card-img');
+
+        let img= document.createElement('img');
+        img.src=item.img;
+
+        let star= document.createElement('i');
+        star.setAttribute('class','bi bi-star-fill');
+        star.setAttribute('id','rating');
+        star.innerText= ' ' + item.rating;
+
+        let heart= document.createElement('ion-icon');
+        heart.setAttribute('class','heart-outline');
+        heart.setAttribute('id',item.id)
+
+        cardTop.appendChild(star);
+        cardTop.appendChild(heart);
+
+        cardImg.appendChild(img);
+
+
+        let itemName= document.createElement('p');
+        itemName.setAttribute('id','item-name');
+        itemName.innerText= item.name;
+
+        let itemPrice= document.createElement('p');
+        itemPrice.setAttribute('id','item-price');
+        itemPrice.innerText= 'Price : $ ' + item.price;
+
+        cardContainer.appendChild(cardTop);
+        cardContainer.appendChild(cardImg);
+        cardContainer.appendChild(itemName);
+        cardContainer.appendChild(itemPrice);
+        
+        appetizer.appendChild(cardContainer);
+
+    })
+
+    dessertData.map(item=>{
+        let cardContainer= document.createElement('div');
+        cardContainer.setAttribute('id','card-container')
+
+        let cardTop= document.createElement('div');
+        cardTop.setAttribute('id','card-top');
+
+        let cardImg= document.createElement('div');
+        cardImg.setAttribute('id', 'card-img');
+
+        let img= document.createElement('img');
+        img.src=item.img;
+
+        let star= document.createElement('i');
+        star.setAttribute('class','bi bi-star-fill');
+        star.setAttribute('id','rating');
+        star.innerText= ' ' + item.rating;
+
+        let heart= document.createElement('ion-icon');
+        heart.setAttribute('class','heart-outline');
+        heart.setAttribute('id',item.id)
+
+        cardTop.appendChild(star);
+        cardTop.appendChild(heart);
+
+        cardImg.appendChild(img);
+
+        let itemName= document.createElement('p');
+        itemName.setAttribute('id','item-name');
+        itemName.innerText= item.name;
+
+        let itemPrice= document.createElement('p');
+        itemPrice.setAttribute('id','item-price');
+        itemPrice.innerText= 'Price : $ ' + item.price;
+
+        cardContainer.appendChild(cardTop);
+        cardContainer.appendChild(cardImg);
+        cardContainer.appendChild(itemName);
+        cardContainer.appendChild(itemPrice);
+        
+        
+        dessert.appendChild(cardContainer);
+    
+    })
+
+    drinksData.map(item=>{
+        let cardContainer= document.createElement('div');
+        cardContainer.setAttribute('id','card-container')
+
+        let cardTop= document.createElement('div');
+        cardTop.setAttribute('id','card-top');
+
+        let cardImg= document.createElement('div');
+        cardImg.setAttribute('id', 'card-img');
+        
+        let img= document.createElement('img');
+        img.src=item.img;
+
+        let star= document.createElement('i');
+        star.setAttribute('class','bi bi-star-fill');
+        star.setAttribute('id','rating');
+        star.innerText= ' ' + item.rating;
+
+        let heart= document.createElement('ion-icon');
+        heart.setAttribute('class','heart-outline');
+        heart.setAttribute('id',item.id)
+
+
+        cardTop.appendChild(star);
+        cardTop.appendChild(heart);
+        
+        cardImg.appendChild(img);
+
+
+        let itemName= document.createElement('p');
+        itemName.setAttribute('id','item-name');
+        itemName.innerText= item.name;
+
+        let itemPrice= document.createElement('p');
+        itemPrice.setAttribute('id','item-price');
+        itemPrice.innerText= 'Price : $ ' + item.price;
+
+        cardContainer.appendChild(cardTop);
+        cardContainer.appendChild(cardImg);
+        cardContainer.appendChild(itemName);
+        cardContainer.appendChild(itemPrice);
+        
+        
+        drinks.appendChild(cardContainer);
+        
+    })
+}
+displayItems();
+
+
+
+
+// Heart toggle 
+function toggleFavorite() {
+    let favoriteIcon = document.getElementById('favorite');
+
+    favoriteIcon.name = (favoriteIcon.name === 'heart-outline') ? 'heart' : 'heart-outline';
+
+    favoriteIcon.classList.toggle('red-heart', favoriteIcon.name === 'heart');
+}
---
+++ Cart/fooditem.js
@@ -0,0 +1,203 @@
+const foodItem = [
+    {
+        id: 1,
+        name: 'Rice with beans',
+        category : 'rice',
+        rating : 4.3,
+        price: 12.94,
+        img : '/images/me/riceandbeans.jpg',
+        quantity: 10
+    },
+        {
+        id: 2,
+        name: 'Rice with porkchops',
+        category : 'rice',
+        rating : 4.2,
+        price: 12.94,
+        img : '/images/menu/dominican-fried-pork-chops.jpg',
+        quantity: 10
+    },
+    {
+        id: 3,
+        name: 'Moro',
+        category : 'rice',
+        rating : 5,
+        price: 11.93,
+        img : '/images/menu/moro.jpg',
+        quantity: 10
+    },
+    {
+        id: 4,
+        name: 'Locrio',
+        category : 'rice',
+        rating : 4,
+        price: 9.54,
+        img : '/images/menu/locrio.jpg',
+        quantity: 10
+    },
+    {
+        id: 5,
+        name: 'Chips and salsa',
+        category : 'appetizer',
+        rating : 4.2,
+        price: 7.89,
+        img : '/images/menu/chips.jpg',
+        quantity: 10
+    },
+    {
+        id: 6,
+        name: 'Tostones y salami',
+        category : 'appetizer',
+        rating : 4.9,
+        price: 6.99,
+        img : '/images/menu/salami.jpg',
+        quantity: 10
+    },
+    {
+        id: 7,
+        name: 'Nachos',
+        category : 'appetizer',
+        rating : 4.7,
+        price: 6.89,
+        img : '/images/menu/nachos.jpg',
+        quantity: 10
+    },
+    {
+        id: 8,
+        name: 'Empanadas',
+        category : 'appetizer',
+        rating : 4.3,
+        price: 4.34,
+        img : '/images/menu/empanadas.jpg',
+        quantity: 10
+    },
+    {
+        id: 9,
+        name: 'Elote',
+        category : 'appetizer',
+        rating : 4.4,
+        price: 5.89,
+        img : '/images/menu/elote.jpg',
+        quantity: 10
+    },
+    {
+        id: 10,
+        name: 'Sancocho',
+        category : 'entree',
+        rating : 4.3,
+        price: 13.98,
+        img : '/images/menu/Sancocho-trifasico.jpg',
+        quantity: 10
+    },
+    {
+        id: 11,
+        name: 'Mangu',
+        category : 'entree',
+        rating : 4.9,
+        price: 10.90,
+        img : '/images/menu/mangu.webp',
+        quantity: 10
+    },
+    {
+        id: 12,
+        name: 'Mofongo',
+        category : 'entree',
+        rating : 4.1,
+        price: 14.58,
+        img : '/images/menu/mofongo.png',
+        quantity: 1
+    },
+    {
+        id: 13,
+        name: 'Chimi',
+        category : 'entree',
+        rating : 4.7,
+        price: 12.32,
+        img : '/images/menu/chimi.jpg',
+        quantity: 10
+    },
+    {
+        id: 14,
+        name: 'Tacos',
+        category : 'entree',
+        rating : 3.9,
+        price: 15.93,
+        img : '/images/menu/tacos.jpg',
+        quantity: 10
+    },
+    {
+        id: 15,
+        name: 'Flan',
+        category : 'dessert',
+        rating : 4.1,
+        price: 8.98,
+        img : '/images/menu/flan.jpg',
+        quantity: 10
+    },
+    {
+        id: 16,
+        name: 'Arroz de dulce',
+        category : 'dessert',
+        rating : 4.2,
+        price: 7.56,
+        img : '/images/menu/leche.jpg',
+        quantity: 10
+    },
+    {
+        id: 17,
+        name: 'Churro',
+        category : 'dessert',
+        rating : 4.9,
+        price: 6.54,
+        img : '/images/menu/churros.jpg',
+        quantity: 10
+    },
+    {
+        id: 18,
+        name: 'Horchata',
+        category : 'drinks',
+        rating : 4.7,
+        price: 3.65,
+        img : '/images/menu/horchata.jpg',
+        quantity: 10
+    },
+    {
+        id: 19,
+        name: 'Jarritos',
+        category : 'drinks',
+        rating : 4.2,
+        price: 2.12,
+        img : '/images/menu/jarritos.jpg',
+        quantity: 10
+    },
+    {
+        id: 20,
+        name: 'Tropical Juice',
+        category : 'drinks',
+        rating : 4.5,
+        price: 2.95,
+        img : '/images/menu/tropical.jpg',
+        quantity: 10
+    },
+    {
+        id: 21,
+        name: 'Pina Colado',
+        category : 'drinks',
+        rating : 4,
+        price: 3.21,
+        img : '/images/menu/pina.jpg',
+        quantity: 10
+    },
+    {
+        id: 22,
+        name: 'Mojito',
+        category : 'dessert',
+        rating : 4.1,
+        price: 1.90,
+        img : '/images/menu/mojito.webp',
+        quantity: 10
+    },
+    
+]
+
+export {foodItem};
